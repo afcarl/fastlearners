870e9d472f50c0920b66b8f10df1823dbcd9d659
Fabien C. Y. Benureau <fabien.benureau+git@gmail.com>
2016-02-07 20:21:47 +0100

    __url__ attribute

--- fastlearners/__init__.py
+++ fastlearners/__init__.py
@@ -5,4 +5,8 @@ __commit__ = get_versions()["full-revisionid"]
 __dirty__ = get_versions()["dirty"]
 del get_versions
 
+__url__ = 'https://github.com/humm/fastlearners'
+
+
+# intra-package imports
 from .fastlearners_pyx import cNNSet, cLWLR
